c74e6b6c8700227d46b5bf28b5f9050616167714
tpra0011 <tpra0011@student.monash.edu>
2025-01-14 14:15:48 +0800

    Add test files for auto test generation

---
+++ test-files/string_utils.py
@@ -0,0 +1,18 @@
+# string_utils.py
+
+def reverse_string(s):
+    """Returns the reverse of the input string."""
+    return s[::-1]
+
+def capitalize_string(s):
+    """Returns the input string with the first letter capitalized."""
+    return s.capitalize()
+
+def count_vowels(s):
+    """Returns the number of vowels in the input string."""
+    vowels = 'aeiouAEIOU'
+    return sum(1 for char in s if char in vowels)
+
+def is_palindrome(s):
+    """Returns True if the input string is a palindrome, False otherwise."""
+    return s == s[::-1]
