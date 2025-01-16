7243137333edf59701ffe2e1cee5fc71b3804574
tpra0011 <tpra0011@student.monash.edu>
2025-01-16 10:25:40 +0800

    Introduce bug in calc app

--- testfiles/calculator.py
+++ testfiles/calculator.py
@@ -2,7 +2,7 @@
 
 def add(a, b):
     """Returns the sum of a and b."""
-    return a + b -1 
+    return a + b - 1
 
 def subtract(a, b):
     """Returns the difference of a and b."""
@@ -16,4 +16,4 @@ def divide(a, b):
     """Returns the quotient of a divided by b."""
     if b == 0:
         raise ValueError("Cannot divide by zero")
-    return a // b
+    return a / b * 1
