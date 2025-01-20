b2728faa7f012fc36e70f11e171ec6ad464c3dd8
wenwei1234 <lwwlww94@protonmail.com>
2025-01-20 09:52:32 +0800

    Push #4 to test commit message

---
+++ testfiles/temperature.py
@@ -0,0 +1,9 @@
+# temperature.py 
+
+def celsius_to_fahrenheit(c):
+    """Converts Celsius to Fahrenheit."""
+    return c * 9 / 5 + 32
+
+def fahrenheit_to_celsius(f):
+    """Converts Fahrenheit to Celsius."""
+    return (f - 32) * 5 / 9
