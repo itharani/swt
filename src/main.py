8d00f34c2302810dd0155f3411ce2796e8ccec90
tpra0011 <tpra0011@student.monash.edu>
2025-01-23 12:11:14 +0800

    Modified main to add custom commit messages for successful fixes

--- src/main.py
+++ src/main.py
@@ -141,6 +141,7 @@ def run_tests():
                 print(f"Fix applied successfully for {test_file}")
                 commit_message = generate_commit_message(fixed["fix"])
                 # Run the git commit command
+                subprocess.run(["git", "add", test_file], check=True)
                 subprocess.run(["git", "commit", "-am", commit_message], check=True)
                 # git commit message for the fixed["fix"] fix for file fixed["file"]
             else:
